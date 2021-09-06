40b9cd3496c71c5169078f0400bdcb9b2575aa2a
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-09-06 18:31:52 +0530

    Commit with Initial Project Code

---
+++ src/test/java/com/yash/dev/rest/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.dev.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
