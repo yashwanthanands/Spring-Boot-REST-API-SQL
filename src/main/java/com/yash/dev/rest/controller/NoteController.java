a6e6709c58c74fc28e3831fc137da136b8f949a3
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-09-07 12:51:02 +0530

    Commit with Get Note Endpoint

--- src/main/java/com/yash/dev/rest/controller/NoteController.java
+++ src/main/java/com/yash/dev/rest/controller/NoteController.java
@@ -6,11 +6,13 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yash.dev.rest.exception.ResourceNotFoundException;
 import com.yash.dev.rest.model.NoteUtil;
 import com.yash.dev.rest.repository.NoteRepository;
 
@@ -34,5 +36,11 @@ public class NoteController {
 	public NoteUtil createNote(@Valid @RequestBody NoteUtil note) {
 		return noteRepository.save(note);
 	}
+	
+	 @GetMapping("/Notes/{id}")
+	    public NoteUtil getNoteById(@PathVariable(value = "id") Long noteId) {
+	        return noteRepository.findById(noteId)
+	                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+	    }
 
 }
